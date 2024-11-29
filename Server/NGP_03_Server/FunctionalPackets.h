2a252303ef35dac977630a6741cdb3cb794bc01e
ahnn001 <ahw8670@gmail.com>
2024-11-29 12:09:19 +0900

    Move_Packet 추가
    Player class의 Move에 대응하는 glm::vec3 move를 전송

--- Server/NGP_03_Server/FunctionalPackets.h
+++ Server/NGP_03_Server/FunctionalPackets.h
@@ -8,3 +8,13 @@ void EventMovePlayer(Player* p, KeyInput& k);
 void EventCreateItem(Item* itemArr);
 void EventCreateBullet(Bullet* bArr);
 void EventMoveBullet(Bullet* bArr);
+
+struct Parent_Packet {
+	byte pakcet_type;
+};
+
+struct Move_Packet : Parent_Packet {
+	byte player_index;
+	glm::vec3 move;
+};
+
